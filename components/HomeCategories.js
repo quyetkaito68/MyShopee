dd85c002882d5ca036ff76210c1519f2f6fe19f2
toankt2k <71335473+toankt2k@users.noreply.github.com>
2021-06-29 20:09:24 +0700

    add component

---
+++ components/HomeCategories.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+
+
+const HomeCategoris = ()=>{
+    return (
+        <View style={style.container}>
+            <Text>toan nguyen</Text>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    container:{
+        backgroundColor:"#00f4",
+        alignItems:'center',
+        marginLeft:10,
+        marginRight:10,
+        minHeight:100,
+    }
+});
+
+
+export default HomeCategoris;
