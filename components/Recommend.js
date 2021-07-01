5fe25de9995e79401b55b4bd23122edd0f3e9248
Nguyễn Văn Quyết <67721370+quyetkaito68@users.noreply.github.com>
2021-07-01 17:33:09 +0700

    add component gợi ý

---
+++ components/Recommend.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import {SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar,Image,useWindowDimensions} from 'react-native';
+import RecommendItem from '../components/RecommendItem';
+
+const DATA = [
+    {
+      id: '1',
+      title: '1 Item',
+      description: '1 Item Description',
+      image: 'https://cf.shopee.co.id/file/765aeb4b74bccb805fbd977000a4536c_tn',
+    },
+    {
+        id: '2',
+        title: '2 Item',
+        description: '2 Item Description',
+        //image: require('../assets/favicon.png'),
+        image: 'https://cf.shopee.co.id/file/57a3f9127fc2d3bae2f40b340832c441_tn',
+      },
+      {
+        id: '3',
+        title: '3 Item',
+        description: '3 Item Description',
+        //image: require('../assets/icon.png'),
+        image: 'https://cf.shopee.co.id/file/9ca203634f979d8a8458356e5358383e_tn',
+      },
+      {
+        id: '4',
+        title: '4 Item',
+        description: '4 Item Description',
+        //image: require('../assets/icon.png'),
+        image: 'https://cf.shopee.co.id/file/69144231e045fed91be0041237e3478b_tn',
+      },
+      {
+        id: '5',
+        title: '5 Item',
+        description: '5 Item Description',
+        //image: require('../assets/icon.png'),
+        image: 'https://cf.shopee.co.id/file/729673dc15fb5a4e151b7c6960b25849_tn',
+      },
+      {
+        id: '6',
+        title: '6 Item',
+        description: '6 Item Description',
+        //image: require('../assets/icon.png'),
+        image: 'https://cf.shopee.co.id/file/729673dc15fb5a4e151b7c6960b25849_tn',
+      },
+      {
+        id: '7',
+        title: '7 Item',
+        description: '7 Item Description',
+        //image: require('../assets/icon.png'),
+        image: 'https://cf.shopee.co.id/file/729673dc15fb5a4e151b7c6960b25849_tn',
+      },
+];
+
+const Recommend=()=> {    
+    return (
+    <View style={styles.container}>
+      <View style={styles.title} >
+        <Text style={styles.recommentTittle}>Gợi ý</Text>
+      </View>
+        <FlatList
+        data={DATA}
+        renderItem={({item})=> <RecommendItem item={item}/>}        
+        numColumns={2}
+        keyExtractor={item => item.id}
+        />
+    </View>
+    );
+
+}
+
+export default Recommend;
+  
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    title:{
+        alignSelf:'flex-start',
+        padding:5,        
+    },
+    recommentTittle:{
+      fontSize:20,
+      fontWeight:'bold',
+      color:'red',      
+    },
+});
+  
---
+++ components/RecommendItem.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+    View,Text,StyleSheet,Dimensions,Image
+} from 'react-native';
+
+const deviceWidth = Dimensions.get('window').width;
+
+const RecommendItem=({item})=> {
+    return (
+        <View style = {[styles.containter]}>
+        <Image source={{uri:item.image}} style={styles.image}></Image>
+        <View style={{flex:0.3}}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.description}>{item.description}</Text>
+
+        </View>
+        </View>
+    );
+}
+
+export default RecommendItem;
+
+const styles = StyleSheet.create({
+    containter:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    image:{
+        //flex:0.7,
+        width:deviceWidth/2,
+        height:deviceWidth/2,
+        resizeMode:'stretch',
+        justifyContent:'center',
+    },
+    title: {
+        fontSize: 32,
+    },
+    description:{
+        fontSize:15,
+    },
+});
